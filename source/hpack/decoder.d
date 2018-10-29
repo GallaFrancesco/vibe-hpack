fa81163eab05df951151c67c12a2542fe3b4e302
GallaFrancesco <francesco.galla3@gmail.com>
2018-10-29 12:45:01 +0100

    lazily evaluated input range

--- source/hpack/decoder.d
+++ source/hpack/decoder.d
@@ -27,7 +27,7 @@ struct HeaderDecoder(T = ubyte[])
 	private {
 		immutable(ubyte)[] m_range;
 		IndexingTable m_table; // only for retrieving data
-		HTTP2HeaderTableField[] m_decoded;
+		HTTP2HeaderTableField m_decoded;
 		HTTP2HeaderTableField[] m_index; // to be appended
 		HTTP2HeaderTableField[] m_noindex;
 	}
@@ -44,38 +44,22 @@ struct HeaderDecoder(T = ubyte[])
 	}
 
 // InputRange specific methods
-	@property bool empty() @safe { return m_decoded.empty; }
+	@property bool empty() @safe { return m_range.empty; }
 
-	@property HTTP2HeaderTableField front() @safe { return m_decoded.front; }
-
-	@property HTTP2HeaderTableField back() @safe { return m_decoded.back; }
+	@property HTTP2HeaderTableField front() @safe { return m_decoded; }
 
 	void popFront() @trusted
 	{
 		enforce!HPACKException(!empty, "Cannot call popFront on an empty HeaderDecoder");
 
-		m_decoded.popFront();
-
 		// advance if data is still available
-		if(!m_range.empty) decode();
+		decode();
 	}
 
-	@property HTTP2HeaderTableField opIndex(int idx) @safe
+	void put(T)(T range) @trusted
 	{
-		assert(idx < m_decoded.length, "Invalid decoder index");
-		return m_decoded[idx];
-	}
-
-	@property HTTP2HeaderTableField[] opSlice(int start, int end) @safe
-	{
-		assert(start >= 0 && end < m_decoded.length, "Invalid decoder slice");
-		return m_decoded[start..end];
-	}
-
-	void put(T)(T range) @safe
-	{
-		static if(is(ElementType!T : char)) m_range ~= cast(ubyte[])range;
-		else m_range ~= range;
+		static if(is(typeof(representation(range)) == immutable(ubyte)[])) m_range = range;
+		else m_range = cast(immutable(ubyte)[])range;
 
 		decode();
 	}
@@ -89,56 +73,53 @@ struct HeaderDecoder(T = ubyte[])
 
 		void decode() @trusted
 		{
-			while(!m_range.empty) {
-				auto bbuf = m_range[0].toBitArray();
-				m_range = m_range[1..$];
+			auto bbuf = m_range[0].toBitArray();
+			m_range = m_range[1..$];
 
-				if(bbuf[0]) {
-					auto res = decodeInteger(bbuf);
-					m_decoded ~= m_table[res];
-				} else {
-					HTTP2HeaderTableField hres;
-					bool update = false;
+			if(bbuf[0]) {
+				auto res = decodeInteger(bbuf);
+				m_decoded = m_table[res];
+			} else {
+				HTTP2HeaderTableField hres;
+				bool update = false;
 
-					if (bbuf[1]) { // inserted in dynamic table
-						auto idx = bbuf.toInteger(2);
-						if(idx > 0) {  // name == table[index].name, value == literal
-							hres.name = m_table[idx].name;
-						} else {   // name == literal, value == literal
-							hres.name = decodeLiteral();
-						}
-						hres.value = decodeLiteral();
-						m_index ~= hres;
-
-					} else if(bbuf[3]) { // NEVER inserted in dynamic table
-						auto idx = bbuf.toInteger(4);
-						if(idx > 0) {  // name == table[index].name, value == literal
-							hres.name = m_table[idx].name;
-						} else {   // name == literal, value == literal
-							hres.name = decodeLiteral();
-						}
-						hres.value = decodeLiteral();
-						m_noindex ~= hres;
-
-					} else if(!bbuf[2]) { // this occourrence is not inserted in dynamic table
-						auto idx = bbuf.toInteger(4);
-						if(idx > 0) {  // name == table[index].name, value == literal
-							hres.name = m_table[idx].name;
-						} else {   // name == literal, value == literal
-							hres.name = decodeLiteral();
-						}
-						hres.value = decodeLiteral();
-
-					} else { // dynamic table size update (bbuf[2] is set)
-						update = true;
-						auto nsize = bbuf.toInteger(3);
-						m_table.updateSize(cast(HTTP2SettingValue)nsize);
+				if (bbuf[1]) { // inserted in dynamic table
+					auto idx = bbuf.toInteger(2);
+					if(idx > 0) {  // name == table[index].name, value == literal
+						hres.name = m_table[idx].name;
+					} else {   // name == literal, value == literal
+						hres.name = decodeLiteral();
 					}
+					hres.value = decodeLiteral();
+					m_index ~= hres;
 
-					if(!update) m_decoded ~= hres;
+				} else if(bbuf[3]) { // NEVER inserted in dynamic table
+					auto idx = bbuf.toInteger(4);
+					if(idx > 0) {  // name == table[index].name, value == literal
+						hres.name = m_table[idx].name;
+					} else {   // name == literal, value == literal
+						hres.name = decodeLiteral();
+					}
+					hres.value = decodeLiteral();
+					m_noindex ~= hres;
+
+				} else if(!bbuf[2]) { // this occourrence is not inserted in dynamic table
+					auto idx = bbuf.toInteger(4);
+					if(idx > 0) {  // name == table[index].name, value == literal
+						hres.name = m_table[idx].name;
+					} else {   // name == literal, value == literal
+						hres.name = decodeLiteral();
+					}
+					hres.value = decodeLiteral();
+
+				} else { // dynamic table size update (bbuf[2] is set)
+					update = true;
+					auto nsize = bbuf.toInteger(3);
+					m_table.updateSize(cast(HTTP2SettingValue)nsize);
 				}
-			}
 
+				if(!update) m_decoded = hres;
+			}
 		}
 
 		size_t decodeInteger(BitArray bbuf) @trusted
@@ -211,7 +192,7 @@ unittest {
 	  */
 	block = [0x41, 0x8c, 0xf1, 0xe3, 0xc2, 0xe5, 0xf2, 0x3a, 0x6b, 0xa0, 0xab, 0x90, 0xf4, 0xff];
 	decoder.put(block);
-	assert(decoder.back.name == ":authority" && decoder.back.value == "www.example.com");
+	assert(decoder.front.name == ":authority" && decoder.front.value == "www.example.com");
 	assert(decoder.toIndex.back.name == ":authority" && decoder.toIndex.back.value == "www.example.com");
 
 	/** 2. Literal header field without indexing (raw)
@@ -219,7 +200,7 @@ unittest {
 	  */
 	block = [0x04, 0x0c, 0x2f, 0x73, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x2f, 0x70, 0x61, 0x74, 0x68];
 	decoder.put(block);
-	assert(decoder.back.name == ":path" && decoder.back.value == "/sample/path");
+	assert(decoder.front.name == ":path" && decoder.front.value == "/sample/path");
 
 
 	/** 3. Literal header field never indexed (raw)
@@ -228,7 +209,7 @@ unittest {
 	block = [0x10, 0x08, 0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64, 0x06, 0x73, 0x65,
 		  0x63, 0x72, 0x65, 0x74];
 	decoder.put(block);
-	assert(decoder.back.name == "password" && decoder.back.value == "secret");
+	assert(decoder.front.name == "password" && decoder.front.value == "secret");
 	assert(decoder.neverIndexed.back.name == "password" && decoder.neverIndexed.back.value == "secret");
 
 
@@ -238,7 +219,7 @@ unittest {
 	import vibe.http.common;
 	block = [0x82];
 	decoder.put(block);
-	assert(decoder.back.name == ":method" && decoder.back.value == HTTPMethod.GET);
+	assert(decoder.front.name == ":method" && decoder.front.value == HTTPMethod.GET);
 
 	/** 5. Full request without huffman encoding
 	  * :method: GET
@@ -250,11 +231,16 @@ unittest {
 	block = [0x82, 0x86, 0x84, 0xbe, 0x58, 0x08, 0x6e, 0x6f, 0x2d, 0x63, 0x61, 0x63, 0x68, 0x65];
 	table.insert(HTTP2HeaderTableField(":authority", "www.example.com"));
 	auto rdec = HeaderDecoder!(ubyte[])(block, table);
-	assert(rdec[0].name == ":method" && rdec[0].value == HTTPMethod.GET);
-	assert(rdec[1].name == ":scheme" && rdec[1].value == "http");
-	assert(rdec[2].name == ":path" && rdec[2].value == "/");
-	assert(rdec[3].name == ":authority" && rdec[3].value == "www.example.com");
-	assert(rdec[4].name == "cache-control" && rdec[4].value == "no-cache");
+	HTTP2HeaderTableField[] expected = [
+		HTTP2HeaderTableField(":method", HTTPMethod.GET),
+		HTTP2HeaderTableField(":scheme", "http"),
+		HTTP2HeaderTableField(":path", "/"),
+		HTTP2HeaderTableField(":authority", "www.example.com"),
+		HTTP2HeaderTableField("cache-control", "no-cache")];
+
+	foreach(i,h; rdec.enumerate(0)) {
+		assert(h == expected[i]);
+	}
 
 	/** 5. Full request with huffman encoding
 	  * :method: GET
@@ -265,9 +251,8 @@ unittest {
 	  */
 	block = [0x82, 0x86, 0x84, 0xbe, 0x58, 0x86, 0xa8, 0xeb, 0x10, 0x64, 0x9c,0xbf];
 	auto hdec = HeaderDecoder!(ubyte[])(block, table);
-	assert(hdec[0].name == ":method" && hdec[0].value == HTTPMethod.GET);
-	assert(hdec[1].name == ":scheme" && hdec[1].value == "http");
-	assert(hdec[2].name == ":path" && hdec[2].value == "/");
-	assert(hdec[3].name == ":authority" && hdec[3].value == "www.example.com");
-	assert(hdec[4].name == "cache-control" && hdec[4].value == "no-cache");
+
+	foreach(i,h; hdec.enumerate(0)) {
+		assert(h == expected[i]);
+	}
 }

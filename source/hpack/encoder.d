95995e9297fee95e46e307e1879ee9e537e8fe9c
GallaFrancesco <francesco.galla3@gmail.com>
2018-10-30 17:08:22 +0100

    finished encoder struct

--- source/hpack/encoder.d
+++ source/hpack/encoder.d
@@ -16,11 +16,12 @@ import std.exception;
 import std.array;
 
 struct HeaderEncoder(T = HTTP2HeaderTableField[])
-	if(isInputRange!T &&
-	is(ElementType!T : HTTP2HeaderTableField))
+	if(isInputRange!T && is(ElementType!T : HTTP2HeaderTableField) ||
+	   is(T == HTTP2HeaderTableField))
 {
 	private {
-		T m_range;
+		static if(is(T == HTTP2HeaderTableField)) T[] m_range;
+		else T m_range;
 		IndexingTable m_table;
 		ubyte[] m_encoded;
 	}
@@ -29,7 +30,9 @@ struct HeaderEncoder(T = HTTP2HeaderTableField[])
 
 	this(T range, IndexingTable table, bool huff = true) @trusted
 	{
-		m_range = range;
+		static if(is(T == HTTP2HeaderTableField)) m_range ~= range;
+		else m_range = range;
+
 		m_table = table;
 		huffman = huff;
 
@@ -73,6 +76,7 @@ struct HeaderEncoder(T = HTTP2HeaderTableField[])
 			if(!indexed) encodeLiteral(header);
 		}
 
+		/// encode a pure integer (present in table) or integer name + literal value
 		bool encodeInteger(const HTTP2HeaderTableField header) @safe
 		{
 			// check table for indexed headers
@@ -99,30 +103,50 @@ struct HeaderEncoder(T = HTTP2HeaderTableField[])
 				// encode name as index, value as literal
 				} else if(h.name == header.name && h.value != header.value) {
 					// encode name as index ( always smaller than 64 )
-					bbuf.put(cast(ubyte)((idx + 64) & 127));
+					if(header.index) bbuf.put(cast(ubyte)((idx + 64) & 127));
+					else if (header.neverIndex) bbuf.put(cast(ubyte)((idx + 16) & 31));
+					else bbuf.put(cast(ubyte)(idx & 15));
 					// encode value as literal
-					if(huffman) {
-						auto hbuf = appender!(ubyte[]); // TODO a proper allocator
-						auto len = encodeHuffman(to!string(header.value), hbuf);
-						bbuf.put(cast(ubyte)((len/8 ^ 128) + 1));
-						bbuf.put(hbuf.data);
-					} else {
-						auto blen = (header.value.length) & 127;
-						bbuf.put(cast(ubyte)blen);
-						bbuf.put(cast(ubyte[])(to!string(header.value).dup));
-					}
+					encodeLiteralField(to!string(header.value), bbuf);
+
 					m_encoded = bbuf.data;
 					return true;
-
 				}
 				idx++;
 			}
 			return false;
 		}
 
-		void encodeLiteral(const HTTP2HeaderTableField header) @safe {
-			assert(false);
+		/// encode a literal field depending on its indexing requirements
+		void encodeLiteral(const HTTP2HeaderTableField header) @safe
+		{
+			auto bbuf = appender!(ubyte[]); // TODO a proper allocatorA
+
+			if(header.index) bbuf.put(cast(ubyte)(64));
+			else if(header.neverIndex) bbuf.put(cast(ubyte)(16));
+			else bbuf.put(cast(ubyte)(0));
+
+			encodeLiteralField(to!string(header.name), bbuf);
+			encodeLiteralField(to!string(header.value), bbuf);
+
+			m_encoded = bbuf.data;
 		}
+
+		/// encode a field (name / value) using huffman or raw encoding
+		void encodeLiteralField(Out)(string src, ref Out dst) @safe
+		{
+			if(huffman) {
+				auto hbuf = appender!(ubyte[]); // TODO a proper allocator
+				auto len = encodeHuffman(src, hbuf);
+				dst.put(cast(ubyte)((len/8 ^ 128) + 1));
+				dst.put(hbuf.data);
+			} else {
+				auto blen = (src.length) & 127;
+				dst.put(cast(ubyte)blen);
+				dst.put(cast(ubyte[])(to!string(src).dup));
+			}
+		}
+
 	}
 }
 
@@ -130,47 +154,50 @@ unittest {
 	// Following examples can be found in Appendix C of the HPACK RFC
 	import HPACK.decoder;
 	IndexingTable table;
+
 	/** 1. Literal header field w. indexing (raw)
 	  * custom-key: custom-header
 	  */
 	// TODO encodeLiteral
-	//HTTP2HeaderTableField[] h1 = [HTTP2HeaderTableField("custom-key", "custom-header")];
-	//auto e1 = HeaderEncoder!(HTTP2HeaderTableField[])(h1, table);
-	//auto d1 = HeaderDecoder!(ubyte[])(e1.front, table);
-	//assert(d1.front == h1.front);
+	HTTP2HeaderTableField h1 = HTTP2HeaderTableField("custom-key", "custom-header");
+	auto e1 = HeaderEncoder!(HTTP2HeaderTableField)(h1, table, false);
+	auto d1 = HeaderDecoder!(ubyte[])(e1.front, table);
+	assert(d1.front == h1);
 
 	/** 1bis. Literal header field w. indexing (huffman encoded)
 	  * :authority: www.example.com
 	  */
-	HTTP2HeaderTableField[] h1b = [HTTP2HeaderTableField(":authority", "www.example.com")];
-	auto e1b = HeaderEncoder!(HTTP2HeaderTableField[])(h1b, table);
+	HTTP2HeaderTableField h1b = HTTP2HeaderTableField(":authority", "www.example.com");
+	auto e1b = HeaderEncoder!(HTTP2HeaderTableField)(h1b, table);
 	auto d1b = HeaderDecoder!(ubyte[])(e1b.front, table);
-	assert(d1b.front == h1b.front);
+	assert(d1b.front == h1b);
 
 	/** 2. Literal header field without indexing (raw)
 	  * :path: /sample/path
 	  */
-	HTTP2HeaderTableField[] h2 = [HTTP2HeaderTableField(":path", "/sample/path")];
+	HTTP2HeaderTableField h2 = HTTP2HeaderTableField(":path", "/sample/path");
 	// initialize with huffman=false (can be modified by e2.huffman)
-	auto e2 = HeaderEncoder!(HTTP2HeaderTableField[])(h2, table, false);
+	auto e2 = HeaderEncoder!(HTTP2HeaderTableField)(h2, table, false);
 	auto d2 = HeaderDecoder!(ubyte[])(e2.front, table);
-	assert(d2.front == h2.front);
+	assert(d2.front == h2);
 
 	/** 3. Literal header field never indexed (raw)
 	  * password: secret
 	  */
 	// TODO encodeLiteral
-	//HTTP2HeaderTableField[] h3 = [HTTP2HeaderTableField("password", "secret")];
-	//auto e3 = HeaderEncoder!(HTTP2HeaderTableField[])(h3, table, false);
-	//auto d3 = HeaderDecoder!(ubyte[])(e3.front, table);
-	//assert(d3.front == h3.front);
+	HTTP2HeaderTableField h3 = HTTP2HeaderTableField("password", "secret");
+	h3.neverIndex = true;
+	h3.index = false;
+	auto e3 = HeaderEncoder!HTTP2HeaderTableField(h3, table, false);
+	auto d3 = HeaderDecoder!(ubyte[])(e3.front, table);
+	assert(d3.front == h3);
 
 	/** 4. Indexed header field (integer) 
 	  * :method: GET
 	  */
-	HTTP2HeaderTableField[] h4 = [HTTP2HeaderTableField(":method", HTTPMethod.GET)];
-	auto e4 = HeaderEncoder!(HTTP2HeaderTableField[])(h4, table);
+	HTTP2HeaderTableField h4 = HTTP2HeaderTableField(":method", HTTPMethod.GET);
+	auto e4 = HeaderEncoder!(HTTP2HeaderTableField)(h4, table);
 	auto d4 = HeaderDecoder!(ubyte[])(e4.front, table);
-	assert(d4.front == h4.front);
+	assert(d4.front == h4);
 
 }
